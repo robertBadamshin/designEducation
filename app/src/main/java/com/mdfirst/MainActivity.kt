47e4e7312a7cf183f94204584da23c10d07120ef
Badamshin Robert <r.badamshin@softpro.com>
2022-01-17 18:54:17 +0300

    show splash with animation

--- app/src/main/java/com/mdfirst/MainActivity.kt
+++ app/src/main/java/com/mdfirst/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.mdfirst
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.*
 import com.mdfirst.dailyimage.ui.DailyImageFragment
-import com.mdfirst.recyclerview.ui.RecyclerViewSampleFragment
 import com.mdfirst.textsamples.ui.TextSampleFragment
+import java.util.concurrent.TimeUnit
+
+private const val hideSplashAnimationDuration = 1000L
 
 class MainActivity : AppCompatActivity() {
+
+    private val splashScreenDuration = TimeUnit.SECONDS.toMillis(3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val splashScreenStartMilliseconds = System.currentTimeMillis()
+        val splashScreen = installSplashScreen()
+
+        val keepSplashOnScreenCondition = SplashScreen.KeepOnScreenCondition {
+            val currentTime = System.currentTimeMillis()
+            val splashOnScreenMilliseconds = currentTime - splashScreenStartMilliseconds
+
+            Log.d("SPLASH", "keepSplashOnScreenCondition")
+
+            splashOnScreenMilliseconds < splashScreenDuration
+        }
+
+        val onExitAnimationListener = SplashScreen.OnExitAnimationListener { splashScreenViewProvider ->
+
+            splashScreenViewProvider
+                .view
+                .animate()
+                .alpha(0f)
+                .setDuration(hideSplashAnimationDuration)
+                .start()
+
+            splashScreenViewProvider
+                .iconView
+                .animate()
+                .alpha(0f)
+                .setDuration(hideSplashAnimationDuration)
+                .start()
+
+            Log.d("SPLASH", "onExitAnimationListener")
+            // splashScreenViewProvider.iconView
+        }
+
+        splashScreen.setOnExitAnimationListener(onExitAnimationListener)
+        splashScreen.setKeepOnScreenCondition(keepSplashOnScreenCondition)
+
         // call before set content view
         setTheme(DailyImageFragment.currentTheme)
-
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
